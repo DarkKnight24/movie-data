35aedc988d7a0ffe24758d5511c77271afafde49
huahua <17638351213@163.com>
2020-03-06 17:53:21 +0800

    添加事物管理

--- src/main/java/com/movie/moviedata/service/impl/MovieServiceImpl.java
+++ src/main/java/com/movie/moviedata/service/impl/MovieServiceImpl.java
@@ -10,7 +10,7 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @Service
-@Transactional
+@Transactional(rollbackFor = Exception.class)
 public class MovieServiceImpl implements MovieService {
 
     @Resource
