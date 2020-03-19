5864fdd74733fbc256e5acce56ed1f622e865f8e
huahua <17638351213@163.com>
2020-03-19 18:05:25 +0800

    查询场次管理

--- src/main/java/com/movie/moviedata/entity/CinemaMovieReleation.java
+++ src/main/java/com/movie/moviedata/entity/CinemaMovieReleation.java
@@ -1,9 +1,9 @@
 package com.movie.moviedata.entity;
 
+import java.util.Date;
+
 import lombok.Data;
 import lombok.experimental.Accessors;
-
-import java.util.Date;
 
 @Data
 @Accessors(chain = true)
@@ -12,37 +12,37 @@ public class CinemaMovieReleation {
      * 主键id
      */
     private Long id;
-
+    
     /**
      * 电影id
      */
     private Long movieId;
-
+    
     /**
      * 影厅类别
      */
     private Long hallType;
-
+    
     /**
      * 影院品牌
      */
     private Long cinemaType;
-
+    
     /**
      * 建议售价
      */
     private Double suggestedPrice;
-
+    
     /**
      * 删除标识
      */
-    private Boolean deleteMark;
-
+    private Boolean deleteMark = false;
+    
     /**
      * 创建时间
      */
     private Date createTime = new Date();
-
+    
     /**
      * 放映时间
      */
---
+++ src/test/java/com/movie/moviedata/service/CinemaMovieReleationServiceTest.java
@@ -0,0 +1,83 @@
+package com.movie.moviedata.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.movie.moviedata.entity.CinemaMovieReleation;
+
+@SpringBootTest
+public class CinemaMovieReleationServiceTest {
+    
+    @Autowired
+    private CinemaMovieReleationService cinemaMovieReleationService;
+    
+    @Autowired
+    private MovieService movieService;
+    
+    @Test
+    public void deleteByPrimaryKey() {
+    }
+    
+    @Test
+    public void insert() {
+    }
+    
+    @Test
+    public void insertOrUpdate() {
+    }
+    
+    @Test
+    public void insertOrUpdateSelective() {
+    }
+    
+    @Test
+    public void insertSelective() {
+    }
+    
+    @Test
+    public void selectByPrimaryKey() {
+    }
+    
+    @Test
+    public void updateByPrimaryKeySelective() {
+    }
+    
+    @Test
+    public void updateByPrimaryKey() {
+    }
+    
+    @Test
+    public void updateBatch() {
+    }
+    
+    @Test
+    public void updateBatchSelective() {
+    }
+    
+    @Test
+    public void batchInsert() {
+        List<CinemaMovieReleation> list = new ArrayList<>();
+        Random random = new Random(20);
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 10; j++) {
+                CinemaMovieReleation cinemaMovieReleation = new CinemaMovieReleation().setCinemaType((long)j)
+                    .setMovieId((long)(7 + i))
+                    .setHallType((long)j)
+                    .setSuggestedPrice(35.6 * (1 + j * 0.45) + random.nextDouble() / 0.5)
+                    .setStartTime(movieService.selectByPrimaryKey((long)(7 + i)).getMovieReleasedate());
+                list.add(cinemaMovieReleation);
+            }
+        }
+        
+        cinemaMovieReleationService.batchInsert(list);
+    }
+    
+    @Test
+    public void selectBySelective() {
+    }
+}
