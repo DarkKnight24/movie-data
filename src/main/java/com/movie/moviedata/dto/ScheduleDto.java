b7f20986f82a1e6e0311db2ae6f0550601487fe6
huahua <17638351213@163.com>
2020-04-22 13:36:25 +0800

    时间格式

--- src/main/java/com/movie/moviedata/dto/ScheduleDto.java
+++ src/main/java/com/movie/moviedata/dto/ScheduleDto.java
@@ -2,6 +2,8 @@ package com.movie.moviedata.dto;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
@@ -15,6 +17,7 @@ public class ScheduleDto {
     
     private String cinemaName;
     
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date startTime;
     
     private double price;
