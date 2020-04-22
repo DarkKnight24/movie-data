f298d69aef901c96acbff74c8af3f3499694cbf1
huahua <17638351213@163.com>
2020-04-22 15:05:38 +0800

    影院查询

--- src/main/java/com/movie/moviedata/param/CinemaSelectParam.java
+++ src/main/java/com/movie/moviedata/param/CinemaSelectParam.java
@@ -4,9 +4,32 @@ import lombok.Data;
 
 /**
  * @作者 ：soul
- * @创建日期 ：Created in 2020/3/28 18:30
- * @描述：
+ * @创建日期 ：Created in 2020/3/28 18:30 @描述：
  */
 @Data
 public class CinemaSelectParam {
+    /**
+     * 上映时间
+     */
+    private String startTime;
+    
+    /**
+     * 电影ID
+     */
+    private Long movieId;
+    
+    /**
+     * 电影品牌ID
+     */
+    private Long cinemaTypeId;
+    
+    /**
+     * 地区ID
+     */
+    private Long areaId;
+    
+    /**
+     * 影厅类别ID
+     */
+    private Long hallTypeId;
 }
